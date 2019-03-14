6762e1c2173bcf8babc4085ea3269f83dc820f5d
mtsgi <oq@live.jp>
2019-03-14 14:11:41 +0900

    Add Selection Sharing

--- bg.js
+++ bg.js
@@ -1,7 +1,14 @@
 chrome.contextMenus.create({
-    "id": "share",
+    id: "share",
     title: "Twitterで共有",
     onclick: openTwitter
+});
+
+chrome.contextMenus.create({
+    id: "context_share",
+    contexts: ["selection"],
+    title: "文字列をツイート",
+    onclick: textTweet
 });
 
 chrome.browserAction.onClicked.addListener( openTwitter );
@@ -12,6 +19,12 @@ function openTwitter(){
         var shareurl = "https://twitter.com/intent/tweet?url=" + prev + "&text=" + tab.title;
         window.open(shareurl, "_blank", "resizable=no,scrollbars=no,status=no,width=400,height=350,menubar=no,top=50,left=50");
     })
+}
+
+function textTweet(info){
+    prev = encodeURI(info.selectionText);
+    var shareurl = "https://twitter.com/intent/tweet?text=" + prev;
+    window.open(shareurl, "_blank", "resizable=no,scrollbars=no,status=no,width=400,height=350,menubar=no,top=50,left=50");
 }
 
 var prev = "";
--- cs.js
+++
@@ -1,5 +0,0 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request == "sendOpen") {
-        alert( sender );
-    }
-}); 
